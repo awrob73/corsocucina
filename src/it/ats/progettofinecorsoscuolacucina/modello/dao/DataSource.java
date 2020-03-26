aba4281d2d712b70efc8d6588c719ec08ea9dc74
Andrea <Andrea@DESKTOP-E7R9V49.lan>
2020-03-26 15:34:00 +0100

    Servlet Login

--- src/it/ats/progettofinecorsoscuolacucina/modello/dao/DataSource.java
+++ src/it/ats/progettofinecorsoscuolacucina/modello/dao/DataSource.java
@@ -39,6 +39,10 @@ public class DataSource {
 		Connection connection;
 		try {
 			connection = dbDriver.connect(url, dbProperties);
+			if(connection==null) {
+				instance.getConnection();
+			}	
+			connection.setAutoCommit(false);
 		} catch (SQLException e) {
 			throw new DAOException(e);
 		}
