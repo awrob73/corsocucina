7ae1c7b067dbc27f914cd7784c4527c011925701
Alexander <Alexander@DESKTOP-AIO19GE.station>
2020-03-31 15:16:46 +0200

    Servlet inserisci categoria

--- src/it/ats/progettofinecorsoscuolacucina/controllo/ServletInserisciCategoria.java
+++ src/it/ats/progettofinecorsoscuolacucina/controllo/ServletInserisciCategoria.java
@@ -1,11 +1,15 @@
 package it.ats.progettofinecorsoscuolacucina.controllo;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import it.ats.progettofinecorsoscuolacucina.modello.Categoria;
 import it.ats.progettofinecorsoscuolacucina.modello.Utente;
 import it.ats.progettofinecorsoscuolacucina.modello.service.ServiceAmministratore;
 import it.ats.progettofinecorsoscuolacucina.modello.service.ServiceCategoria;
@@ -15,7 +19,7 @@ import it.ats.progettofinecorsoscuolacucina.modello.service.ServiceCategoria;
  */
 public class ServletInserisciCategoria extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private ServiceAmministratore sa;
 	private ServiceCategoria sc;
 
@@ -23,29 +27,36 @@ public class ServletInserisciCategoria extends HttpServlet {
 		super();
 		this.sa = ServiceAmministratore.getInstance();
 		this.sc = ServiceCategoria.getInstance();
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String username = request.getParameter("username");
 		String descrizione = request.getParameter("descrizione");
-		
+
 		try {
-			if(!descrizione.trim().isEmpty()) {
-			sc.aggiungiCategoria(descrizione);}
-			Utente a = sa.cercaAmministratorePerUsername(username);
-			
-			request.setAttribute("descrizione", descrizione);
-			request.setAttribute("user", a);
-			
+			if (descrizione.trim().isEmpty()) {
+				List<Categoria> lista = new ArrayList<Categoria>();
+				lista = sc.cercaTutteCategorie();
+				
+				request.setAttribute("listaCategoria", lista);
+				getServletContext().getRequestDispatcher("/WEB-INF/listaCategoria.jsp").forward(request, response);
+			} else {
+				sc.aggiungiCategoria(descrizione);
+				Utente a = sa.cercaAmministratorePerUsername(username);
 
-			getServletContext().getRequestDispatcher("/WEB-INF/confermaNuovaCategoria.jsp").forward(request,
-					response);
+				request.setAttribute("descrizione", descrizione);
+				request.setAttribute("user", a);
 
+				getServletContext().getRequestDispatcher("/WEB-INF/confermaNuovaCategoria.jsp").forward(request,
+						response);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			getServletContext().getRequestDispatcher("/WEB-INF/error.jsp").forward(request, response);
